d21485f0aed32068358bbeae14e74cbd9708d196
Saurabh Kumar <129621065+SaurabhKr002@users.noreply.github.com>
2025-02-24 13:20:05 +0530

    Saurabh add . H2 database connnectivity

---
+++ EmployeePayrollApp/src/main/java/com/bridgelabz/employeepayrollapp/repository/EmployeeRepository.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.employeepayrollapp.repository;
+
+import com.bridgelabz.employeepayrollapp.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+}
---
+++ EmployeePayrollApp/src/main/java/com/bridgelabz/employeepayrollapp/service/EmployeeService.java
@@ -0,0 +1,31 @@
+package com.bridgelabz.employeepayrollapp.service;
+
+import com.bridgelabz.employeepayrollapp.model.Employee;
+import com.bridgelabz.employeepayrollapp.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class EmployeeService {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+
+    public Optional<Employee> getEmployeeById(Long id) {
+        return employeeRepository.findById(id);
+    }
+
+    public Employee addEmployee(Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    public void deleteEmployee(Long id) {
+        employeeRepository.deleteById(id);
+    }
+}
