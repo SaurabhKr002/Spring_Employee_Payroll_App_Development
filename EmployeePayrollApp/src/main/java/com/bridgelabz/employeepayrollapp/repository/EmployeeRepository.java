a64c6973c1d3d30b2f8111e1eaa7e8f2932218d7
Saurabh Kumar <129621065+SaurabhKr002@users.noreply.github.com>
2025-02-28 14:57:12 +0530

    Saurabh Add . Ability to save employee data

--- EmployeePayrollApp/src/main/java/com/bridgelabz/employeepayrollapp/repository/EmployeeRepository.java
+++ EmployeePayrollApp/src/main/java/com/bridgelabz/employeepayrollapp/repository/EmployeeRepository.java
@@ -2,7 +2,6 @@ package com.bridgelabz.employeepayrollapp.repository;
 
 import com.bridgelabz.employeepayrollapp.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 }
